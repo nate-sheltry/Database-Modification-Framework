13abe0fdd64b02e31a541560934549d7853f9e53
Nathaniel Dunham <dun19007@byui.edu>
2026-01-24 11:21:56 -0700

    Add project files.

---
+++ Database Modification Framework/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Modification_Framework
+{
+    public class Main
+    {
+    }
+}
